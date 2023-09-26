a08adda50185cf6bcf31ca7e59e254bae5519f6d
JYKIMkentech <129360396+JYKIMkentech@users.noreply.github.com>
2023-09-27 02:15:12 +0900

    Add files via upload

---
+++ Comparison_DCIR/diffusion_R2.m
@@ -0,0 +1,18 @@
+clc;clear;close all;
+
+load('dcir_fit2')
+load('BigIC2.mat')
+
+% load('dcir_fit3')
+% load('BigIC3.mat')
+
+load('optimized2.mat')
+load('optimized3.mat')
+
+load('Resistance2_time.mat')
+load('Resistance3_time.mat')
+
+
+for i = 1:length(BigIC)
+ R_d2(i) = data(BigIC(i)).R30s - optimized_params2(i).R1 - optimized_params2(i).R2;
+end
